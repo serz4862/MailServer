6e9cb2615dfe44abe2c6fe0be84d882e543dcc54
saurav kumar <64458729+serz4862@users.noreply.github.com>
2024-03-01 22:56:50 +0530

    mail auth and optional

--- Email.js
+++ Email.js
@@ -1,5 +1,7 @@
 const SMTPServer = require("smtp-server").SMTPServer;
 const server = new SMTPServer({
+  allowInsecureAuth: true,
+  authOptional: true,
   onConnect(session,cb){
     console.log(`onConnect`, session.id)
     cb(); //Accept the connection
@@ -7,6 +9,15 @@ const server = new SMTPServer({
   },
   onMailFrom(address, session, cb){
     console.log(`onMailFrom`, address.address, session.id)
+    cb();
+  },
+  onRcptTo(address, session, cb){
+    console.log(`onRcptTo`, address.address, session.id)
+    cb();
+  },
+  onData(stream, session, cb){
+    stream.on('data',(data) => console.log(`onData ${data.toString()}`))
+    stream.on('end', cb)
   }
 });
 
